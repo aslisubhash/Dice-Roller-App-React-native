66a38623ff9041f5d55e964de76da601459b845e
Subhash Chaudhary <myearning4me@gmail.com>
2020-11-27 14:00:17 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,111 @@
+import React, {useState} from "react";
+import {Text, View, Image, Pressable, StyleSheet} from "react-native";
+import DiceOne from "./assets/dice1.png";
+import DiceTwo from "./assets/dice2.png";
+import DiceThree from "./assets/dice3.png";
+import DiceFour from "./assets/dice4.png";
+import DiceFive from "./assets/dice5.png";
+import DiceSix from "./assets/dice6.png";
+
+const App = () => {
+  const [uri, setUri] = useState(DiceOne);
+  const [uri2, setUri2] = useState(DiceFour);
+
+
+  const playButtonTapped =()=>{
+    let randomNumber = Math.floor(Math.random() * 6 +1);
+    let randomNumber2 = Math.floor(Math.random() * 6 +1);
+  
+
+    switch (randomNumber) {
+      case 1:
+        setUri(DiceOne)
+
+        break;
+      case 2:
+        setUri(DiceTwo)
+        break;
+      case 3:
+        setUri(DiceThree)
+        break;
+      case 4:
+        setUri(DiceFour)
+        break;
+      case 5:
+        setUri(DiceFive)
+        break;
+      case 6:
+        setUri(DiceSix)
+        break;
+    
+      default:
+        break;
+    };
+    switch (randomNumber2) {
+      case 1:
+        setUri2(DiceOne)
+
+        break;
+      case 2:
+        setUri2(DiceTwo)
+        break;
+      case 3:
+        setUri2(DiceThree)
+        break;
+      case 4:
+        setUri2(DiceFour)
+        break;
+      case 5:
+        setUri2(DiceFive)
+        break;
+      case 6:
+        setUri2(DiceSix)
+        break;
+    
+      default:
+        break;
+    }
+    
+  }
+  return (
+    <>
+    <View style={styles.container}>
+      <Image style={styles.images} source={ uri } />
+      <Image style={styles.images} source={ uri2 } />
+      <Pressable onPress={playButtonTapped}>
+        <Text style={styles.gamePlayButton}>Play Game</Text>
+      </Pressable>
+      
+      </View>
+    
+    </>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#303c85",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  images: {
+    width: 200,
+    height: 200
+  },
+  gamePlayButton: {
+    fontSize: 20,
+    marginTop: 10,
+    borderRadius: 10,
+    color: "#ffffff",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderColor: "#ff3456",
+    borderWidth: 5,
+    fontWeight: "bold"
+    
+
+  }
+})
